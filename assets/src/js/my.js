dafd4bebf3609c4c864b843a59d48aea62fd0159
«Marina» <gunko.mail@gmail.com>
2021-11-01 21:22:53 +0300

    container-fluid, index/projects

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -1,16 +1,3 @@
-$(document).ready(function(){
-    $(".fa-search").click(function(){
-      $(".fa-search, form").toggle();
-    });
-  
-    $("#close-s").click(function(){
-      $(".fa-search, form").toggle();
-      $("form")[0].reset(); /* put #search or .searchBar */
-    });
-    
-  });
-
-
 //Slider
 
 
